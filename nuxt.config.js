19880c59c8393290d297cd4b37285397666191c7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-11-12 12:15:33 +0000

    add license, favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,17 +15,17 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Open Source Bazaar',
+    title: 'Event Poster Generator - Amman Valley MakerSpace',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Generate posters for events based on open-source software.' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
